6ac7044203c8804a85bd0297b6ff30c56d2882a6
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-04-24 11:31:07 +0530

    Basic routes

---
+++ app/routes/signup.jsx
@@ -0,0 +1,11 @@
+import  { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+
+export default function IndexRoute() {
+  return (
+    <div className="text-4xl font-bold  mb-6">
+      <h1>SIGN UP</h1>
+    </div>
+  );
+}
