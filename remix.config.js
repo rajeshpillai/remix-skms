e1a3fb842e34a1b4ebe71731d11e4c4ca8233882
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-04-24 14:27:23 +0530

    Cleanup

--- remix.config.js
+++ remix.config.js
@@ -10,18 +10,6 @@ module.exports = {
   publicPath: "/build/",
 };
 
-
-// module.exports = {
-//   ignoredRouteFiles: ["**/.*"],
-//   future: {
-//     v2_errorBoundary: true,
-//     v2_normalizeFormMethod: true,
-//     // v2_meta: true,
-//     // v2_routeConvention: true,
-//   },
-// };
-
-
 // module.exports = {
 //   // ignoredRouteFiles: ["**/.*"],
 //   // cacheDirectory: "./node_modules/.cache/remix",
