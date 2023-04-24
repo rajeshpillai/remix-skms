8f436b7c5fe6f5d326e8e4ad3ccd4446b49dbbac
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-04-24 11:54:27 +0530

    Add unocss attributify preset

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig, presetUno } from "unocss";
 
+import presetAttributify from '@unocss/preset-attributify';
 export default defineConfig({
     // more presets: https://uno.antfu.me/?s=preset
     presets: [
       presetUno(),
+      presetAttributify({ /* preset options */ }),
     ]
 });
